cd90e86abef4c2085e5508e33adc28675ce5c7d9
Your Name <your-email@example.com>
2025-04-25 03:22:27 +0530

    done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const mongooseconnection = require("./config/mongoose");
 const khataModel = require("./models/khata");
-
+require("dotenv").config();
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -114,6 +114,6 @@ app.get("/delete/:filename", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("App listening on port http://localhost:3000");
 });
